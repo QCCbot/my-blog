bd46d346d16625e0e9451f187df4c7eca5379f73
QCCBot Cloud Phone <testuser10@qccbot.com>
2026-04-08 15:38:12 +0800

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'QCCBot Cloud Phone';
+export const SITE_DESCRIPTION = 'The Next Generation Cloud Phone Solution';
